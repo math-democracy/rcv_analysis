886612b28354d30159939606abad92d9c7ecc8bd
Karen Xiao <xiaokaren@Karens-MacBook-Pro.local>
2024-12-12 13:35:35 -0500

    Upload processed australian data

--- australia/aus_driver.py
+++ australia/aus_driver.py
@@ -1,7 +1,7 @@
 import os
-from rcv_proposal.australia.aus_parse import parser
+from aus_parse import parser
 
-root_dir = '/Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw/Australia_Victoria_LegAssembly_2018' # UPDATE TO WHERE BLT DATA IS STORED eg. /Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw/Australia_Victoria_LegAssembly_2022
+root_dir = '/Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw_australian_data/Australia_NSW_LegAssembly_ByElections_Miscellaneous' # UPDATE TO WHERE BLT DATA IS STORED eg. /Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw/Australia_Victoria_LegAssembly_2022
 output_folder = '/Users/xiaokaren/MyPythonCode/ranked_choice_voting/processed/' # UPDATE TO WHERE CSV DATA SHOULD BE SAVED eg. /Users/xiaokaren/MyPythonCode/ranked_choice_voting/processed/
 output_folder += root_dir.split('/')[-1]
 
@@ -9,7 +9,7 @@ for dirpath, dirnames, filenames in os.walk(root_dir):
     for filename in filenames:
         if filename.endswith('.zip') or filename.endswith('.xlsx'):
             full_path = os.path.join(dirpath, filename)
-            if 'NewSouthWales' in full_path:
+            if 'NewSouthWales' in full_path or 'NSW' in full_path:
                 state = 'New South Wales'
             elif 'Victoria' in full_path:
                 state = 'Victoria'
