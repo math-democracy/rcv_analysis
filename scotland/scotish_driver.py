bda142c9ce69faf7eae0c01c767e6b00fea89982
Belle <bellesee1212@gmail.com>
2024-12-16 16:43:43 -0500

    data

--- scotland/scotish_driver.py
+++ scotland/scotish_driver.py
@@ -1,8 +1,8 @@
 import os
-from parse import parser
+from scotland_parser import parser
 
-root_dir = '' # UPDATE TO WHERE BLT DATA IS STORED eg. /Users/belle/Desktop/build/rcv/shetland22
-output_folder = '' # UPDATE TO WHERE CSV DATA SHOULD BE SAVED eg. /Users/belle/Desktop/build/rcv
+root_dir = '' # UPDATE TO WHERE BLT DATA IS STORED eg. /Users/belle/Downloads/Scotland data, LEAP parties
+output_folder = '' # UPDATE TO WHERE CSV DATA SHOULD BE SAVED eg. /Users/belle/Desktop/build/rcv_proposal/data
 
 for dirpath, dirnames, filenames in os.walk(root_dir):
     for filename in filenames:
