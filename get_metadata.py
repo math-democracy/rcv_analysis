49f08afff860cad79538648a017b5fee7eb0eae9
Belle <bellesee1212@gmail.com>
2024-12-29 12:34:46 -0500

    more data

---
+++ get_metadata.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import csv
+import os
+import random
+
+root_dir = '/Users/belle/Downloads/Scotland data, LEAP parties' # UPDATE TO WHERE BLT DATA IS STORED eg. /Users/belle/Downloads/Scotland data, LEAP parties
