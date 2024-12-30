d7923f15aa1649f9072728497314e2fec3dc28a4
xiaokaren <xiaokaren@gmail.com>
2024-12-30 09:02:53 -0700

    add numSeats and numCands to aus data

--- australia/aus_driver.py
+++ australia/aus_driver.py
@@ -1,8 +1,8 @@
 import os
 from aus_parse import parser
 
-root_dir = '/Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw_australian_data/Australia_NSW_LegAssembly_ByElections_Miscellaneous' # UPDATE TO WHERE BLT DATA IS STORED eg. /Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw/Australia_Victoria_LegAssembly_2022
-output_folder = '/Users/xiaokaren/MyPythonCode/ranked_choice_voting/processed/' # UPDATE TO WHERE CSV DATA SHOULD BE SAVED eg. /Users/xiaokaren/MyPythonCode/ranked_choice_voting/processed/
+root_dir = '/Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw_australian_data/Australia_Victoria_LegAssembly_2022' # UPDATE TO WHERE BLT DATA IS STORED eg. /Users/xiaokaren/MyPythonCode/ranked_choice_voting/raw/Australia_Victoria_LegAssembly_2022
+output_folder = '/Users/xiaokaren/MyPythonCode/ranked_choice_voting/rcv_proposal/australia/processed_data/' # UPDATE TO WHERE CSV DATA SHOULD BE SAVED eg. /Users/xiaokaren/MyPythonCode/ranked_choice_voting/processed/
 output_folder += root_dir.split('/')[-1]
 
 for dirpath, dirnames, filenames in os.walk(root_dir):
