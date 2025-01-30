fca887351ce7a74084a70642dd6cad22d08790f1
Malavika Mukundan <malavikamukundan@crc-dot1x-nat-10-239-253-104.bu.edu>
2025-01-30 15:15:54 -0500

    update

--- main_methods.py
+++ main_methods.py
@@ -17,8 +17,9 @@ import pref_voting.voting_methods as p #
 
 #cleaning and other logistics
 from votekit.cleaning import remove_noncands
+from votekit.ballot import Ballot
 from votekit.pref_profile import PreferenceProfile
-
+from votekit.graphs import PairwiseComparisonGraph
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
 #profile conversion for both votekit and pref_voting packages
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -30,8 +31,26 @@ def v_profile(filename, to_remove = ["undervote", "overvote", "UWI"]):
 #pref_voting
 def p_profile(filename):
     return creator.create_profile(filename)
-    
+#
+# Head-to-Head counts for a votekit profile - borrowed from votekit.PairwiseComparisonGraph
+#  
 
+def head2head_count(
+    prof: PreferenceProfile, 
+    cand_1: str, 
+    cand_2: str
+):
+    count = 0
+    bal = prof.ballots
+    for b in bal:
+        rank_list = b.ranking
+        for s in rank_list:
+            if cand_1 in s:
+                count += b.weight
+                break
+            elif cand_2 in s:
+                break
+    return count
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
 #Plurality - can choose package
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -107,27 +126,27 @@ def Borda_PM(
 
     max_score=len(prof.candidates)-1 ##this will be equal to len(non_UWI_cands)-1 if UWI not in cands_to_keep, and to len(cands_to_keep)-1 if UWI is in cands_to_keep
     ballots = prof.ballots
-    el_scores= {c:0 for c in cands}
+    el_scores= {c:0 for c in cands_to_keep}
     for i in range(1,len(prof.candidates)+1):
         bal = [b for b in ballots if len(b.ranking) == i]
         for b in bal:
             cands_in_b = []
-            for c in cands:
+            for c in cands_to_keep:
                 for y in b.ranking:
                     if c in y:
                         cands_in_b.append(c)
                         break
-            not_in_b = [{c} for c in cands if c not in cands_in_b]
+            not_in_b = [{c} for c in cands_to_keep if c not in cands_in_b]
             b=Ballot(ranking = list(b.ranking) + not_in_b, weight = b.weight)
-            vector = list(range(max_score,max_score-i,-1))+[0 for k in range(len(cands)-i)] 
+            vector = list(range(max_score,max_score-i,-1))+[0 for k in range(len(cands_to_keep)-i)] 
         p = PreferenceProfile(ballots = bal)
         el = v.Borda(profile = p, score_vector = vector).election_states[0].scores
-        for c in cands:
+        for c in cands_to_keep:
             if c not in el:
                 el[c]=0
             el_scores[c]+=el[c]
     winning_score = max(el_scores.values())
-    elected = [c for c in cands if el_scores[c]==winning_score]
+    elected = [c for c in cands_to_keep if el_scores[c]==winning_score]
     return elected
     
 #Borda OM
@@ -145,27 +164,27 @@ def Borda_OM(
 
     max_score=len(prof.candidates)-1 
     ballots = prof.ballots
-    el_scores= {c:0 for c in cands}
+    el_scores= {c:0 for c in cands_to_keep}
     for i in range(1,len(prof.candidates)+1):
         bal = [b for b in ballots if len(b.ranking) == i]
         for b in bal:
             cands_in_b = []
-            for c in cands:
+            for c in cands_to_keep:
                 for y in b.ranking:
                     if c in y:
                         cands_in_b.append(c)
                         break
-            not_in_b = [{c} for c in cands if c not in cands_in_b]
+            not_in_b = [{c} for c in cands_to_keep if c not in cands_in_b]
             b=Ballot(ranking = list(b.ranking) + not_in_b, weight = b.weight)
-            vector = list(range(max_score,max_score-i,-1))+[max_score-i for k in range(len(cands)-i)] 
+            vector = list(range(max_score,max_score-i,-1))+[max_score-i for k in range(len(cands_to_keep)-i)] 
         p = PreferenceProfile(ballots = bal)
         el = v.Borda(profile = p, score_vector = vector).election_states[0].scores
-        for c in cands:
+        for c in cands_to_keep:
             if c not in el:
                 el[c]=0
             el_scores[c]+=el[c]
     winning_score = max(el_scores.values())
-    elected = [c for c in cands if el_scores[c]==winning_score]
+    elected = [c for c in cands_to_keep if el_scores[c]==winning_score]
     return elected
 
 
@@ -185,27 +204,27 @@ def Borda_AVG(
 
     max_score=len(prof.candidates)-1 
     ballots = prof.ballots
-    el_scores= {c:0 for c in cands}
+    el_scores= {c:0 for c in cands_to_keep}
     for i in range(1,len(prof.candidates)+1):
         bal = [b for b in ballots if len(b.ranking) == i]
         for b in bal:
             cands_in_b = []
-            for c in cands:
+            for c in cands_to_keep:
                 for y in b.ranking:
                     if c in y:
                         cands_in_b.append(c)
                         break
-            not_in_b = [{c} for c in cands if c not in cands_in_b]
+            not_in_b = [{c} for c in cands_to_keep if c not in cands_in_b]
             b=Ballot(ranking = list(b.ranking) + not_in_b, weight = b.weight)
-            vector = list(range(max_score,max_score-i,-1))+[(max_score-i)/2 for k in range(len(cands)-i)] 
+            vector = list(range(max_score,max_score-i,-1))+[(max_score-i)/2 for k in range(len(cands_to_keep)-i)] 
         p = PreferenceProfile(ballots = bal)
         el = v.Borda(profile = p, score_vector = vector).election_states[0].scores
-        for c in cands:
+        for c in cands_to_keep:
             if c not in el:
                 el[c]=0
             el_scores[c]+=el[c]
     winning_score = max(el_scores.values())
-    elected = [c for c in cands if el_scores[c]==winning_score]
+    elected = [c for c in cands_to_keep if el_scores[c]==winning_score]
     return elected
 
 
@@ -236,71 +255,101 @@ def Top3Truncation(
     return elected
     
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
-#Condorcet methods - we use pref_voting
+#Condorcet methods - we use votekit
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
 
-#regular Condorcet: returns Condorcet winner if there exists once, else returns everyone
+#regular Condorcet: returns Condorcet winner if there exists once, else returns []
 def Condorcet(
     filename: str
 ):
-    prof, file_path, candidates_with_indices = p_profile(filename)
-    el = p.condorcet(prof)
-    elected = [candidates_with_indices[c] for c in el]
+    prof= v_profile(filename)
+    elected = list(v.DominatingSets(profile = prof).election_states[-1].elected[0])[0]
+    if len(elected)>1:
+        elected = []
     return elected
 
+
+#Smith set: returns Smith set as a list
+def Smith(
+    filename: str
+):
+    prof= v_profile(filename)
+    elected = list(v.DominatingSets(profile = prof).election_states[-1].elected[0])[0]
+    return elected
+
+#Smith Plurality
+def Smith_Plurality(
+    filename: str
+):
+    prof= v_profile(filename)
+    smith = list(v.DominatingSets(profile = prof).election_states[-1].elected[0])[0]
+    if len(smith)<len(prof.candidates):
+        noncands = [c for c in prof.candidates if c not in smith]
+        prof = remove_noncands(prof, noncands) 
+    elected = list(v.Plurality(profile = prof).election_states[-1].elected[0])[0]
+    return elected
+
+
+#Smith-IRV
+def Smith_IRV(
+    filename: str
+):
+    prof= v_profile(filename)
+    smith = list(v.DominatingSets(profile = prof).election_states[-1].elected[0])[0]
+    if len(smith)<len(prof.candidates):
+        noncands = [c for c in prof.candidates if c not in smith]
+        prof = remove_noncands(prof, noncands) 
+    elected = list(v.IRV(profile = prof).election_states[-1].elected[0])[0]
+    return elected
 
 
 #minimax: returns list of minimax winners
 def Minimax(
     filename: str
 ):
-    prof, file_path, candidates_with_indices = p_profile(filename)
-    el = p.minimax(prof)
-    elected = [candidates_with_indices[c] for c in el]
+    prof= v_profile(filename)
+    cands = [c for c in prof.candidates if c!="skipped"]
+    defeat_margins={c:0 for c in cands}
+    for c in cands:
+        defeat_margins[c] = 0
+        for y in cands:
+            margin = head2head_count(prof,y,c) - head2head_count(prof,c,y)
+            if margin> defeat_margins[c]:
+                    defeat_margins[c] = margin
+    elected = [c for c in defeat_margins if defeat_margins[c]==min(defeat_margins.values())]          
     return elected
     
-
-#Smith set: returns Smith set as a list
-def Smith(
-    filename: str
-):
-    prof, file_path, candidates_with_indices = p_profile(filename)
-    el = p.top_cycle(prof)
-    elected = [candidates_with_indices[c] for c in el]
-    return elected
-
-
-#Smith-IRV: this one is causing errors if we just use pref_voting, so I modified it by 
-def Smith_IRV(
-    filename: str
-):
-    prof, file_path, candidates_with_indices = p_profile(filename)
-    el = p.top_cycle(prof)
-    elected = [candidates_with_indices[c] for c in p.instant_runoff_for_truncated_linear_orders(prof, curr_cands=el)]
-    return elected
-
 
 #Smith-minimax
-def Smith_minimax(
+def Smith_Minimax(
     filename: str
 ):
-    prof, file_path, candidates_with_indices = p_profile(filename)
-    el = p.smith_minimax(prof)
-    elected = [candidates_with_indices[x] for x in el]
+    prof= v_profile(filename)
+    smith = list(v.DominatingSets(profile = prof).election_states[-1].elected[0])[0]
+    if len(smith)<len(prof.candidates):
+        noncands = [c for c in prof.candidates if c not in smith]
+        prof = remove_noncands(prof, noncands) 
+    cands = [c for c in prof.candidates if c!="skipped"]
+    defeat_margins={c:0 for c in cands}
+    for c in cands:
+        defeat_margins[c] = 0
+        for y in cands:
+            margin = head2head_count(prof,y,c) - head2head_count(prof,c,y)
+            if margin>defeat_margins[c]:
+                    defeat_margins[c] = margin
+    elected = [x for x in defeat_margins if defeat_margins[x]==min(defeat_margins.values())]          
     
     return elected
 
+
 #ranked pairs
-def Ranked_Pairs(
-    filename: str
-):
-    prof, file_path, candidates_with_indices = p_profile(filename)
-    el = p.ranked_pairs(prof)
-    elected = [candidates_with_indices[c] for c in el]
-    return elected
-
-
-#Condorcet plurality
+#def Ranked_Pairs(
+ #   filename: str
+#):
+#    prof, file_path, candidates_with_indices = p_profile(filename)
+#    el = p.ranked_pairs(prof)
+#    elected = [candidates_with_indices[c] for c in el]
+#   return elected
 
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
@@ -309,6 +358,7 @@ def Ranked_Pairs(
 
 def Bucklin(
     filename: str,
+    
 ):
     elected =[]
     prof = v_profile(filename)
@@ -329,7 +379,27 @@ def Bucklin(
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
 #Approval
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
+def Approval(
+    filename: str,
+    cands_to_keep: list[str]
+):
+    prof= v_profile(filename)
+    if 'skipped' in cands_to_keep:## remove 'skipped' from cands_to_keep
+        cands_to_keep = list(filter(lambda c: c != 'skipped', cands_to_keep))
+        
+    if len(cands_to_keep)<len(prof.candidates):
+        noncands = [c for c in prof.candidates if c not in cands_to_keep]
+        prof = remove_noncands(prof, noncands) ##at this point prof only has cands_to_keep. Will include UWI iff UWI is in cands_to_keep. This profile has no 'skipped' positions
 
+    ballots = prof.ballots
+    el_scores= {c:0 for c in cands_to_keep}
+    for b in ballots:
+        for s in b.ranking:
+            for c in list(s):
+                el_scores[c]+=b.weight
+    winning_score = max(el_scores.values())
+    elected = [c for c in cands_to_keep if el_scores[c]==winning_score]
+    return elected
 
 #--------------------------------------------------------------------------------------------------------------------------------------------------#
 #Others
